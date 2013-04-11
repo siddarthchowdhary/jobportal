e8230d8f3105d93bf1771a4ae5ffa889fd615833
siddarth chowdhary <siddarth.chowdhary@osscube.com>
2013-04-11 19:39:35 +0530

    crap deleted from job-seeker related

--- library/pdo/pdo_config.php
+++ library/pdo/pdo_config.php
@@ -2,7 +2,7 @@
 /* Example Config for MYSQL */
 $config = array();
 $config['user'] = 'root';
-$config['pass'] = 'mysql';
+$config['pass'] = 'root';
 $config['name'] = 'job_portal';
 $config['host'] = 'localhost';
 $config['type'] = 'mysql';
